43ca276249e373b4f568e9fe0da60f172738b792
Volcano0 <34512910+704817030@users.noreply.github.com>
2022-05-03 20:08:30 +0800

    Add files via upload

---
+++ NorthData/HttpUtil.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System.IO;
+namespace NorthData
+{
+    public class HttpUtil
+    {
+        public static string Get(string Url)
+        {
+            string retString = string.Empty;
+            //System.GC.Collect();
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Proxy = null;
+                request.KeepAlive = false;
+                request.Method = "GET";
+                request.ContentType = "application/json; charset=UTF-8";
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+                retString = myStreamReader.ReadToEnd();
+
+                myStreamReader.Close();
+                myResponseStream.Close();
+
+                if (response != null)
+                {
+                    response.Close();
+                }
+                if (request != null)
+                {
+                    request.Abort();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.ToString());
+                return "";
+            }
+            return retString;
+        }
+
+
+        private static readonly string DefaultUserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受   
+        }
+
+        public HttpWebResponse CreatePostHttpResponse(string url, IDictionary<string, string> parameters, Encoding charset)
+        {
+            HttpWebRequest request = null;
+            //HTTPSQ请求
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            request = WebRequest.Create(url) as HttpWebRequest;
+            request.ProtocolVersion = HttpVersion.Version10;
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = DefaultUserAgent;
+            //如果需要POST数据   
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                int i = 0;
+                foreach (string key in parameters.Keys)
+                {
+                    if (i > 0)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                    }
+                    i++;
+                }
+                byte[] data = charset.GetBytes(buffer.ToString());
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        public string Post(string Url, string Data, string Referer)
+        {
+            string retString = string.Empty;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = "POST";
+                request.Referer = Referer;
+                byte[] bytes = Encoding.UTF8.GetBytes(Data);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = bytes.Length;
+                Stream myResponseStream = request.GetRequestStream();
+                myResponseStream.Write(bytes, 0, bytes.Length);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader myStreamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                retString = myStreamReader.ReadToEnd();
+
+                myStreamReader.Close();
+                myResponseStream.Close();
+
+                if (response != null)
+                {
+                    response.Close();
+                }
+                if (request != null)
+                {
+                    request.Abort();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.ToString());
+                return string.Empty;
+            }
+            return retString;
+        }
+
+        /// 创建POST方式的HTTP请求  
+        public static HttpWebResponse GetPostHttpResponse(string url, IDictionary<string, string> parameters, int timeout, string userAgent, CookieCollection cookies)
+        {
+            HttpWebRequest request = null;
+            //如果是发送HTTPS请求  
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+            }
+            else
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+            }
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded;charset=utf8";
+
+            //设置代理UserAgent和超时
+            //request.UserAgent = userAgent;
+            //request.Timeout = timeout; 
+
+            if (cookies != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+            }
+            //发送POST数据  
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                int i = 0;
+                foreach (string key in parameters.Keys)
+                {
+                    if (i > 0)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                        i++;
+                    }
+                }
+                //byte[] data = Encoding.ASCII.GetBytes(buffer.ToString());
+                byte[] data = Encoding.UTF8.GetBytes(buffer.ToString());
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            string[] values = request.Headers.GetValues("Content-Type");
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        /// <summary>
+        /// 获取请求的数据
+        /// </summary>
+        public static string GetResponseString(HttpWebResponse webresponse)
+        {
+            using (Stream s = webresponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(s, Encoding.UTF8);
+                return reader.ReadToEnd();
+
+            }
+        }
+    }
+
+}
---
+++ NorthData/NorthDataJSON.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace NorthData
+{
+    public class Data
+    {
+        public string SECUCODE { get; set; }
+        public string MUTUAL_TYPE { get; set; }
+        public string TRADE_DATE { get; set; }
+        public string INTERVAL_TYPE { get; set; }
+        public string SECURITY_NAME { get; set; }
+        public string SECURITY_INNER_CODE { get; set; }
+        public string ORG_CODE { get; set; }
+        public string SECURITY_CODE { get; set; }
+        public string PARTICIPANT_NUM { get; set; }
+        public string EFFECTIVE_DATE { get; set; }
+        public string A_SHARES_RATIO { get; set; }
+        public string HOLD_SHARES_RATIO { get; set; }
+        public string HOLD_SHARES { get; set; }
+        public string HOLD_MARKET_CAP { get; set; }
+        public string FREE_SHARES_RATIO { get; set; }
+        public string TOTAL_SHARES_RATIO { get; set; }
+        public string CLOSE_PRICE { get; set; }
+        public string CHANGE_RATE { get; set; }
+        public string INDUSTRY_CODE { get; set; }
+        public string INDUSTRY_NAME { get; set; }
+        public string ORIG_INDUSTRY_CODE { get; set; }
+        public string CONCEPT_CODE { get; set; }
+        public string CONCEPT_NAME { get; set; }
+        public string AREA_CODE { get; set; }
+        public string AREA_NAME { get; set; }
+        public string ORIG_AREA_CODE { get; set; }
+        public string FREECAP { get; set; }
+        public string TOTAL_MARKET_CAP { get; set; }
+        public string FREECAP_HOLD_RATIO { get; set; }
+        public string TOTAL_MARKETCAP_HOLD_RATIO { get; set; }
+        public string ADD_MARKET_CAP { get; set; }
+        public string ADD_SHARES_REPAIR { get; set; }
+        public string ADD_SHARES_AMP { get; set; }
+        public string FREECAP_RATIO_CHG { get; set; }
+        public string TOTAL_RATIO_CHG { get; set; }
+        public string HOLD_MARKETCAP_BEFORECHG { get; set; }
+        public string HOLD_SHARES_BEFORECHG { get; set; }
+        public string HOLD_MARKETCAP_CHG1 { get; set; }
+        public string HOLD_MARKETCAP_CHG5 { get; set; }
+        public string HOLD_MARKETCAP_CHG10 { get; set; }
+        public string INDUSTRY_CODE_NEW { get; set; }
+        public string AREA_CODE_NEW { get; set; }
+    }
+
+    public class Result
+    {
+        public string pages { get; set; }
+        public List<Data> data { get; set; }
+        public string count { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string version { get; set; }
+        public Result result { get; set; }
+        public string success { get; set; }
+        public string message { get; set; }
+        public string code { get; set; }
+    }
+    class NorthDataJSON
+    {
+        public RootObject NorthDataRoot = null;
+        public string GetPureNorthDataJSON(string strJSON)
+        {
+            if (strJSON.Trim().Length == 0)
+                return strJSON.Trim();
+            //找到第一个( ;
+            int nFirstPos = strJSON.IndexOf("(");
+
+            int nLastPos = strJSON.LastIndexOf(")");
+            string strTemp = strJSON.Substring(nFirstPos + 1, nLastPos - nFirstPos - 1);
+            return strTemp;
+        }
+        public bool GetNorthDataDataByJSON(string strJSON)
+        {
+            RootObject oro = JsonConvert.DeserializeObject<RootObject>(strJSON);
+            if (oro.message != "ok")
+            {
+                Console.WriteLine("No data.");
+                return false;
+            }
+            this.NorthDataRoot = oro;
+            Result rs = oro.result;
+            string strPages = rs.pages;
+            int nPages = Convert.ToInt16(strPages); //页数
+            int nCount = Convert.ToInt16(rs.count); //总股票条数
+
+            for (int i = 0; i < rs.data.Count; i++)
+            {
+                Data data = rs.data[i];
+                /*   Console.WriteLine(" 日期 " + data.DATE + " 收盘价 " + data.SPJ + " 涨跌幅 " + data.ZDF + " scode " + data.SCODE + " sname " + data.SECNAME +
+                                     " 融资余额 " + data.RZYE + " 融资余额占流通比 " + data.RZYEZB + " 融资买入额 " + data.RZMRE +
+                                     " 融资偿还额 " + data.RZCHE + " 融资净买入 " + data.RZJME + " 融券余额 " + data.RQYE +
+                                     " 融券余量 " + data.RQYL + " 融券卖出量 " + data.RQMCL + " 融券偿还额 " + data.RQCHL + " 融券净卖出 " + data.RQJMG +
+                                     " 融资融券余额 " + data.NorthDataYE + " 融资融券余额差值 " + data.NorthDataYECZ);
+                                     */
+            }
+
+
+            return true;
+        }
+    }
+}
